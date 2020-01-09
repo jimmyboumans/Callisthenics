45f808c5ccd5745c4baad464f5d3a7731d55bc15
UnDodoAmorphe <dev.undodoamorphe@gmail.com>
2020-01-09 15:45:37 +0100

    Méthodes account

--- src/main/java/main/Account.java
+++ src/main/java/main/Account.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Date;
 
 public class Account {
 	private Integer totalAmount;
@@ -8,5 +9,20 @@ public class Account {
 		this.totalAmount = 0;
 	}
 	
+	public Boolean deposit (Amount amount, Date date) {
+		try {
+			
+		}
+		catch(Error error) {
+			System.err.println("Le programme a rencontré");
+		}
+		this.totalAmount += amount;
+	}
+	
+	public Boolean withdrawal (Amount amount, Date date) {
+		if (this.totalAmount >= amount)
+			this.totalAmount -= amount;
+		
+	}
 	
 }
